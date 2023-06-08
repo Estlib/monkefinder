6f2d839c807fb986feb400c58f9282b1bd2fd695
Estlib <75094683+Estlib@users.noreply.github.com>
2023-06-08 18:50:38 +0300

    monke, lot of monke

---
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,47 @@
+﻿namespace MonkeyFinder.ViewModel;
+public partial class BaseViewModel : ObservableObject
+{
+    public BaseViewModel() 
+    {
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    //public bool IsBusy 
+    //{ 
+    //    get => isBusy; 
+    //    set
+    //    {
+    //        if (isBusy != value)
+    //            return;
+    //        isBusy = value;
+    //        OnPropertyChanged(); 
+    //        OnPropertyChanged(nameof(IsNotBusy)); 
+    //    }   
+    //}
+
+    //public string Title
+    //{
+    //    get => title;
+    //    set
+    //    {
+    //        if (title != value)
+    //            return;
+    //        title = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+
+    public bool IsNotBusy => !IsBusy;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
